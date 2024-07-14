9e3e004b2db880c854e6146b7bdc8d4df32d3ff9
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2024-07-14 21:08:14 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import "./Header.css";
 
-
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header-contents">
-                <h2>Order Your Favourite Food Here</h2>
-                <p>Lorem Impsum Build Full Stack Food Ordering Website using React JS, MongoDB, Express, Node JS & Strip</p>
-                <button>View Menu</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          FoodieGo is a food delivery application that aims to connect users
+          with their favorite restaurants and eateries
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
